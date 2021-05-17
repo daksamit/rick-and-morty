fc2758fa225fbf9cad359db0ec82cad536371aae
Dominik Aksamit <daksamit@daksamit.pl>
2021-05-17 19:32:25 +0200

    feat: use @apply for container styling

--- pages/index.vue
+++ pages/index.vue
@@ -48,18 +48,8 @@ export default defineComponent({
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
 .container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 
 .title {
